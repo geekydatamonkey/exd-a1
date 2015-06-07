e79f0bcf342f514d2b2929d8a80e97533cf48ce6
James Johnson <johnsojr@gmail.com>
2015-06-06 21:32:11 -0500

    Some collision detection

--- app/scripts/particle.js
+++ app/scripts/particle.js
@@ -7,11 +7,22 @@ const _  = require('lodash');
 class Particle {
 
   constructor(config) {
+
     let defaults = {
       position: { x: 0, y: 0},
       radius: 1,
       velocity: {dx: 0, dy: 0}
     };
+
+    // assume args are position if given 2 arguments
+    if (arguments.length === 2) {
+      config = {
+        position: {
+          x: arguments[0],
+          y: arguments[1]
+        }
+      }
+    }
 
     // merge defaults with config object
     config =  _.assign({}, defaults, config);
@@ -30,16 +41,9 @@ class Particle {
     return this;
   }
 
-  setVelocity(vector) {
-    let v = this.velocity;
-    if (Array.isArray(vector)){
-      v.dx = vector[0];
-      v.dy = vector[1];
-    } else {
-      v.dx = vector.dx;
-      v.dy = vector.dy;
-    }
-
+  setVelocity(dx, dy) {
+    this.velocity.dx = dx;
+    this.velocity.dy = dy;
     return this;
   }
 
@@ -47,10 +51,58 @@ class Particle {
   * current position as Array
   * @return Array
   **/
-  getCoordinates() {
+  getCenter() {
     return [this.position.x, this.position.y];
   }
 
+  setPosition(x,y) {
+    this.position.x = x;
+    this.position.y = y;
+    return this;
+  }
+
+  getRadius() {
+    return this.radius;
+  }
+
+  setRadius(r) {
+    this.radius = r;
+  }
+
+  distanceTo(x,y) {
+
+    // if x is an object, then another particle
+    // was given, so we find the center
+    if (x instanceof Particle) {
+      let p = x;
+      x = p.position.x;
+      y = p.position.y;
+    }
+
+    let deltaX = x - this.position.x;
+    let deltaY = y - this.position.y;
+    return Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+  }
+
+  /**
+  * checks if this particle is colliding with another
+  * given particle.
+  **/
+  isCollidingWith(p2) {
+    // no particle collides with itself
+    if (this === p2) {
+      return false;
+    }
+    // the distance between the center of this particle
+    // and the center of p2 must be greater than
+    // the radius of this + the radius of p2
+    // if it's not colliding
+    let d = this.distanceTo(p2);
+    let r1 = this.getRadius();
+    let r2 = p2.getRadius();
+    //console.log(`${d} <=? ${r1} + ${r2}`);
+    return this.distanceTo(p2) <= this.radius + p2.radius;
+  }
 
 }
 
--- test/spec/particle.spec.js
+++ test/spec/particle.spec.js
@@ -44,6 +44,11 @@
       expect(p2.radius).to.deep.equal(p.radius);
     });
 
+    it('should allow initial position to be set at instantiation', function() {
+      let p1 = new Particle(-5,10);
+      expect(p1.position).to.deep.equal({x:-5, y:10});
+    });
+
     describe('update position', function(){
       it('should not change by default since theres no initial velocity', function(){
         let p2 = new Particle();
@@ -51,7 +56,7 @@
         expect(p2.position).to.deep.equal(p.position);
       });
       it('should move horizontally correctly', function(){
-        p.setVelocity([1,0]);
+        p.setVelocity(1,0);
         expect(p.velocity).to.deep.equal({dx: 1, dy: 0});
 
         p.update();
@@ -65,7 +70,7 @@
 
       });
       it('should move vertically correctly', function() {
-        p.setVelocity({dx: 0, dy: 1});
+        p.setVelocity(0, 1);
         expect(p.velocity).to.deep.equal({dx: 0, dy: 1});
 
         p.update();
@@ -80,11 +85,58 @@
       });
 
       it('should handle diagonal movements too', function(){
-        p.setVelocity({dx: 3, dy: 4});
+        p.setVelocity(3, 4);
         p.update(5);
-        expect(p.getCoordinates()).to.deep.equal([15,20]);
+        expect(p.getCenter()).to.deep.equal([15,20]);
       });
+
+      xit('should fail if setVelocity gets invalid args', function() {
+        let fn = p.setVelocity.bind(p); 
+        // bind() undefined on PhantomJS?
+        expect(fn).to.throw(TypeError);
+      })
     });
+
+    describe('collision detection', function() {
+      it('should measure distance between center and a given point',function(){
+
+        let d = Math.sqrt(5*5 + 5*5);
+        expect(p.distanceTo(5,5)).to.equal(d);
+
+        // between (-5,10) and (4, 7.5)
+        let dx = (4 - (-5));
+        let dy = (7.5 - 10);
+        d = Math.sqrt(dx*dx + dy*dy);
+
+        let p1 = new Particle(-5,10);
+        let p2 = new Particle(4,7.5);
+        expect(p1.distanceTo(p2)).to.equal(d);
+
+
+      })
+      it('should check if its colliding with another particle', function() {
+
+        // no particle collides with itself
+        expect(p.isCollidingWith(p)).to.be.false;
+
+        // (0,0) and (0,0) but distinct particles
+        let p2 = new Particle();
+        expect(p.isCollidingWith(p2)).to.be.true;
+
+        // (0,0) and (1,1)
+        let p3 = new Particle();
+        p3.setPosition(1,1);
+        expect(p.isCollidingWith(p3)).to.be.true;
+
+        // (-3,5) and (5,3)
+        let p4 = new Particle();
+        p4.setPosition(-3,5);
+        let p5 = new Particle();
+        p5.setPosition(5,3);
+        expect(p4.isCollidingWith(p5)).to.be.false;
+
+      });
+    })
 
 
   }); // end describe('particle')
